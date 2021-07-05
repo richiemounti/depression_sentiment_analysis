20a76079a9e29c9a1700b5dad2b89d1bc8af9ae6
riciemounti <babtdaddy67@gmail.com>
2021-07-05 16:06:30 +0300

    removed creds

--- Download_twitter_Api.py
+++ Download_twitter_Api.py
@@ -3,10 +3,10 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 
 
-consumer_key = 'GySFfOZkz9ze4XaNGhFojWWkD'
-consumer_secret = '7FnEwAhcA4xtGXnjfpcH5Kg9jGfJPjtsNYI4dai1FG42tUL450'
-access_token = '1282609786958368768-42nHwO6apCPONXqTfQ1FJVJ0Hh6TWj'
-access_secret = 'OWk9iyV5VlTs8N8OzUngrYpVeLkq1b16n7Y825yneHCv1'
+consumer_key = ''
+consumer_secret = ''
+access_token = '-'
+access_secret = ''
 
 
 
